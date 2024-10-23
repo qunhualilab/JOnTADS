90542bb5f0d203a471f75e86fc1c59048cfa3486
Qiuhai Zeng <71804132+Jason-Qiuhai@users.noreply.github.com>
2024-10-23 10:47:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='JOnTADS',
-    version='0.3',
+    version='0.4',
     author='Qiuhai Zeng',
     author_email='qiuhai.stat@gmail.com',
     description='JOnTADS: a unified caller for TADs and stripes in Hi-C data',
